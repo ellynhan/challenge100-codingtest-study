d78ad0bd8d4720dbe2403be5590dd349c7748daa
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2021-09-15 23:52:51 +0900

    하는중

---
+++ jaewon/cutLog.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std; 
+
+#define ll long long
+//1142~
+int getDist(pair<int,int> a, pair<int, int> b){
+    return pow(a.first-b.first,2)+pow(a.second-b.second,2);
+}
+
+int main(void) {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL); 
+    int n, goal;
+    cin >> n >> goal;
+
+    vector<int> trees;
+    int maxHeight = 1;
+    for(int i=0; i<n; i++){
+        int tmp;
+        cin >> tmp;
+        trees.push_back(tmp);
+        if(tmp>maxHeight)maxHeight = tmp;
+    }
+    sort(trees.rbegin(), trees.rend());
+    
+    
+    int minHeight = 0;
+    int mid = (maxHeight+minHeight)/2;
+    ll total = 0;
+    int answer = 0;
+    while(maxHeight>minHeight){
+        for(int i=0; i<n&&trees[i]>mid; i++){
+            if(total>=goal){
+                if(answer>mid){
+                    answer = mid;
+                }
+                minHeight = mid+1;
+                break;
+            }
+            total += trees[i]-mid;
+        }
+        if(total<goal){
+            maxHeight = mid-1;
+        }
+    }
+    cout<<answer;
+}
+
