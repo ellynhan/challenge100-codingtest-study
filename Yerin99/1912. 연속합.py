58095b5c5f880a88fe8c1c0ec94ee01f115776c8
Yerin99 <diluny2@naver.com>
2021-10-06 19:47:20 +0900

    [BOJ] 연속합 / 실버2 / 3시간
    https://www.acmicpc.net/problem/1912

--- Yerin99/1912. 연속합.py
+++ Yerin99/1912. 연속합.py
@@ -2,24 +2,9 @@ import sys
 
 n = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().split()))
-sums = [0]
+sums = [nums[0]]
 
-for i in range(n):
-    sums.append(sums[-1] + nums[i])
+for i in range(1, n):
+    sums.append(nums[i] + max(sums[-1], 0))
 
-maxes = [sums[-1]]
-
-for i in range(n):
-    maxes.append(max(maxes[-1], sums[-(2+i)]))
-
-maxes = maxes[::-1]
-
-min_sum = 1000
-max_sum = max(maxes[1:])
-
-for i in range(n):
-    if sums[i] < min_sum:
-        min_sum = sums[i]
-        max_sum = max(max_sum, maxes[i+1]-sums[i])
-
-print(max_sum)
+print(max(sums))
