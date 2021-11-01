35c04f40cddb07598b88cc8526c0d20b2a3bab5c
PNU-Seung <ksgg1navercom@gmail.com>
2021-11-01 20:49:10 +0900

    [BOJ] 이중 우선순위 큐 / 골드5 / 18분
    https://www.acmicpc.net/problem/7662

---
+++ seunggil/7662.cpp
@@ -0,0 +1,43 @@
+﻿#include <set>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int testCase;
+	cin >> testCase;
+
+	multiset<int> priorityQueue;
+
+	for (int i = 0; i < testCase; i++) {
+		priorityQueue.clear();
+		int n;
+		cin >> n;
+
+		char ord;
+		int num;
+		for (int j = 0; j < n; j++) {
+			cin >> ord >> num;
+
+			if (ord == 'I')
+				priorityQueue.insert(num);
+			else if (ord == 'D' && priorityQueue.size()) {
+				if (num == 1)
+					priorityQueue.erase(--priorityQueue.end());
+				else
+					priorityQueue.erase(priorityQueue.begin());
+			}
+		}
+
+		if (priorityQueue.size())
+			cout << *(--priorityQueue.end()) << ' ' << *priorityQueue.begin() << '\n';
+		else
+			cout << "EMPTY" << '\n';
+	}
+
+	return 0;
+}
