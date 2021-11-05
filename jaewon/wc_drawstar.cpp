a51b51d8858a85bda40db42097eb0baeb39dcb05
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2021-11-06 00:36:51 +0900

    [PGS] 위클리챌린지_교점에 별 만들기 / level2 / 1일
    https://programmers.co.kr/learn/courses/30/lessons/87377

--- jaewon/wc_drawstar.cpp
+++ jaewon/wc_drawstar.cpp
@@ -1,14 +1,7 @@
-#include <iostream>
 #include <string>
 #include <vector>
 #include <map>
 using namespace std;
-/*
-ax+by+f = 0
-cx+dy+e = 0
-x = bf-ed/ad-bc
-y = ec-af/ad-bc
-*/
 vector<string> solution(vector<vector<int>> line) {
     vector<string> answer;
     map<pair<int,int>,int> m;
@@ -16,20 +9,29 @@ vector<string> solution(vector<vector<int>> line) {
     int yMinMax[2]={INT32_MAX,-INT32_MAX};
     for(int i=0; i<line.size()-1; i++){
         for(int j=i+1; j<line.size(); j++){
-            if(line[i][0]*line[j][1]-line[j][0]*line[i][1]==0)continue;
-            double x = (double)(line[i][1]*line[j][2]-line[i][2]*line[j][1])/(line[i][0]*line[j][1]-line[j][0]*line[i][1]);
-            double y = (double)(line[i][2]*line[j][0]-line[i][0]*line[j][2])/(line[i][0]*line[j][1]-line[j][0]*line[i][1]);
-            if(x-int(x)!=0 || y-int(y)!=0)continue;
+            long long a = line[i][0];
+            long long b = line[i][1];
+            long long c = line[j][0];
+            long long d = line[j][1];
+            long long e = line[i][2];
+            long long f = line[j][2];
+            long long adbc = a*d-b*c;
+            if(adbc==0)continue;
+            long long bfed = b*f-e*d;
+            long long ecaf = e*c-a*f;
+            if(bfed%adbc!=0 || ecaf%adbc!=0)continue;
+            int x = bfed/adbc;
+            int y = ecaf/adbc;
             m[{x,-y}]=1;
-            xMinMax[0] = min(xMinMax[0],(int)x);
-            xMinMax[1] = max(xMinMax[1],(int)x);
-            yMinMax[0] = min(yMinMax[0],-(int)y);
-            yMinMax[1] = max(yMinMax[1],-(int)y);
+            xMinMax[0] = min(xMinMax[0],x);
+            xMinMax[1] = max(xMinMax[1],x);
+            yMinMax[0] = min(yMinMax[0],-y);
+            yMinMax[1] = max(yMinMax[1],-y);
         }
     }
-    for(long long i=yMinMax[0]; i<=yMinMax[1]; i++){
+    for(int i=yMinMax[0]; i<=yMinMax[1]; i++){
         string s="";
-        for(long long j=xMinMax[0]; j<=xMinMax[1]; j++){
+        for(int j=xMinMax[0]; j<=xMinMax[1]; j++){
             if(m.count({j,i})>0){
                 s+="*";
             }else{
