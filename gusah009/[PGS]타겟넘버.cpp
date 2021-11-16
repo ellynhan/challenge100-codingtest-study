cb984df52af39f4f85f8c76472ba112c94febba5
gram_jhm <gusah009@naver.com>
2021-11-16 16:58:16 +0900

    [PGS] 타겟넘버 / - / -

---
+++ gusah009/[PGS]타겟넘버.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, j) for(int i = 0; i < j; i++)
+
+void input()
+{
+
+}
+
+void solve()
+{
+
+}
+
+void print()
+{
+
+}
+
+int main()
+{
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  input();
+  solve();
+  print();
+}
