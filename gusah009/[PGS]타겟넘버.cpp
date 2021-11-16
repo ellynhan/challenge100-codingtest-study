3e0fe90e476eca69d058fcfd52dc2e1f8ed34b22
gram_jhm <gusah009@naver.com>
2021-11-16 17:20:27 +0900

    [PGS] 타겟넘버 / - / -

--- gusah009/[PGS]타겟넘버.cpp
+++ gusah009/[PGS]타겟넘버.cpp
@@ -5,19 +5,43 @@ using namespace std;
 
 #define FOR(i, j) for(int i = 0; i < j; i++)
 
+int N;
+int numbers[21] = {0};
+int target;
+int answer = 0;
+
 void input()
 {
+  cin >> N;
+  FOR(n, N) {
+    cin >> numbers[n];
+  }
+  cin >> target;
+}
 
+void DFS(int sum, int depth)
+{
+  if (depth == N - 1) {
+    if (sum == target) {
+      answer++;
+    }
+    return;
+  }
+
+  DFS(sum + numbers[depth + 1], depth + 1);
+  DFS(sum - numbers[depth + 1], depth + 1);
+  
 }
 
 void solve()
 {
-
+  DFS(numbers[0], 0);
+  DFS(-numbers[0], 0);
 }
 
 void print()
 {
-
+  cout << answer << '\n';
 }
 
 int main()
