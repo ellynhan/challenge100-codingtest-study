8af8db3c7142b17230e24d468e663026f771c2bc
ellynhan <wown252@naver.com>
2022-01-15 20:21:53 +0900

    [BOJ] 비밀번호 찾기 / 실버4 / 6분

---
+++ jaewon/17219_findPw.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#define ioboost ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main(){
+    ioboost;
+    int n,m;
+    cin >> n >> m;
+    map<string, string> M;
+    
+    for(int i=0; i<n; i++){
+        string address, pw;
+        cin >> address >> pw;
+        M[address] = pw;
+    }
+
+    for(int i=0; i<m; i++){
+        string address;
+        cin >> address;
+        cout<<M[address]<<'\n';
+    }
+}
