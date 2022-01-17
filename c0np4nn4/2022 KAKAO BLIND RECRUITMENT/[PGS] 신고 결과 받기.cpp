6eec21c0127d3b9bf83f2c6d883045b1c4e646de
c0np4nn4 <49471288+c0np4nn4@users.noreply.github.com>
2022-01-17 19:46:17 +0900

    [PGS] 신고 결과 받기 / Level 1 / 82분
    https://programmers.co.kr/learn/courses/30/lessons/92334

---
+++ c0np4nn4/2022 KAKAO BLIND RECRUITMENT/[PGS] 신고 결과 받기.cpp
@@ -0,0 +1,44 @@
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    vector<int> answer;
+    
+    bool report_info[1001][1001];
+    int report_count[1001];
+    
+    int user_len = id_list.size();
+    
+    answer.resize(user_len, 0);
+
+    
+    // 1. count how many time each user is reported
+    for (int i = 0; i < report.size(); i++) {
+        vector<string> report_str = split(report[i], ' ');
+        
+        vector<string>::iterator from, to;
+        from = find(id_list.begin(), id_list.end(), report_str[0]);
+        to = find(id_list.begin(), id_list.end(), report_str[1]);
+        
+        int i_from, i_to;
+        i_from = distance(id_list.begin(), from);
+        i_to = distance(id_list.begin(), to);
+        
+        if (report_info[i_from][i_to] != true) {
+            report_info[i_from][i_to] = true;
+            report_count[i_to]++;
+        }
+    }
+    
+    
+    // 2. check if user reported the banned one
+    for (int i = 0; i < user_len; i++) {
+        int cnt = 0;
+        for (int j = 0; j < user_len; j++) {
+            if (report_info[i][j] && report_count[j] >= k) {
+                // answer[i]++;
+                cnt++;
+            }
+        }
+        answer[i] = cnt;
+    }
+    
+    return answer;
+}
