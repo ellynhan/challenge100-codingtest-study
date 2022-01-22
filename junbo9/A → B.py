85f6d827154f045600d9cbdcd77d36b04b347273
junbo9 <96970186+junbo9@users.noreply.github.com>
2022-01-22 14:14:48 +0900

    "[BOJ] Hello World / 실버1 / 1시간"
    https://www.acmicpc.net/problem/16953
    도저히 감이 안잡혀서 해설 봄

---
+++ junbo9/A → B.py
@@ -0,0 +1,15 @@
+a,b = map(int,input().split())
+r = 1
+while(b!=a):
+    r+=1
+    temp = b
+    if b%10 == 1:
+        b//=10
+    elif b%2 == 0:
+        b//=2
+    
+    if temp == b:
+        print(-1)
+        break
+else:
+    print(r)
