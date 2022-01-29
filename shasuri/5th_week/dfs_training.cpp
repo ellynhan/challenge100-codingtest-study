9f42c77f11c4586bdf2f49a154436987c48320ff
shasuri <ghimmk@gmail.com>
2022-01-29 18:22:29 +0900

    [SEA] 12317. 기초 DFS 연습 / ??? / 1시간
    이전과 같이 주어진 함수를 요구하는 대로 구현하는 문제. dfs_init()과 dfs()를 구현했다.
    입력으로 트리를 받고, dfs로 n보다 큰 수를 찾으면 큰 수, 못찾으면 -1을 반환하면 되는 문제였다.
    이때까지 재귀로만 dfs를 구현했으나 직접 스택을 만들어서 구현하는 법을 배워 적용시켜보았다.
    dfs함수가 끝나고 stack이 클리어 되지 않아서 값이 틀린 부분을 소멸자를 생성해서 처리했다.

---
+++ shasuri/5th_week/dfs_training.cpp
@@ -0,0 +1,74 @@
+#define MAX_NSIZE 40
+#define UNIQUE_ABILITY 100
+
+using namespace std;
+
+int dynasty[UNIQUE_ABILITY][MAX_NSIZE];
+int childNum[UNIQUE_ABILITY];
+
+class ChildStack
+{
+private:
+    int container[MAX_NSIZE];
+    int top = -1;
+
+public:
+    ChildStack(void);
+    ~ChildStack(void);
+    void push(int);
+    int pop(void);
+};
+
+ChildStack::ChildStack() {}
+ChildStack::~ChildStack()
+{
+    this->top = -1;
+}
+
+void ChildStack::push(int value)
+{
+    if (top >= MAX_NSIZE)
+        return;
+    container[++(this->top)] = value;
+}
+
+int ChildStack::pop(void)
+{
+    if (top <= -1)
+        return -1;
+    return container[(this->top)--];
+}
+
+void dfs_init(int N, int path[100][2])
+{
+    int i;
+    for (i = 0; i < UNIQUE_ABILITY; i++)
+        childNum[i] = 0;
+
+    for (i = 0; i < N - 1; i++)
+    {
+        // cout << path[i][0] << " : " << path[i][1] << endl;
+        dynasty[path[i][0]][childNum[path[i][0]]] = path[i][1];
+        childNum[path[i][0]]++;
+    }
+}
+
+int dfs(int n)
+{
+    ChildStack childStack;
+    int head = n;
+    while (true)
+    {
+        for (int i = childNum[head] - 1; i > -1; i--)
+        {
+            // cout << "head : " << head << " i : " << i << endl;
+            childStack.push(dynasty[head][i]);
+        }
+
+        head = childStack.pop();
+        if (head > n || head == -1)
+            break;
+    }
+    // cout << head << endl;
+    return head;
+}
