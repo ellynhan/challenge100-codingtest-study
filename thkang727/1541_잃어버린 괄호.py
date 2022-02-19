66af52985334667e3509d4fcf2facb1496f75dd7
taehwan Kang <65062771+thkang727@users.noreply.github.com>
2022-02-19 16:34:36 +0900

    Update 1541_잃어버린 괄호.py

--- thkang727/1541_잃어버린 괄호.py
+++ thkang727/1541_잃어버린 괄호.py
@@ -6,17 +6,27 @@
 #
 
 str_eval= input().split('-')
-num = []
+#print(str_eval)
+
+num=[]
+sum=0
+
 for _ in str_eval:
-    cnt = 0
-    s = _.split('+')
-    for j in s:
-        cnt += int(j)
-    num.append(cnt)
-n = num[0]
-for i in range(1, len(num)):
-    n -= num[i]
-print(n)
-
-            
-
+    
+    plus_cnt=0
+    tmp = 0
+    for j in _:
+        if j =='+':
+            plus_cnt+=1
+    if plus_cnt>0:
+        str_tmp = _.split('+')
+        for j in str_tmp:
+            tmp+=int(j)
+        num.append(tmp)
+    else:
+        num.append(int(_))
+#print(num)
+sum = num[0]
+for _ in range(1,len(num)):
+    sum-=num[_]
+print(sum)
