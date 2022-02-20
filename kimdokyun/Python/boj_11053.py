ec3e870aab52eaf39e5114d13164ab0c9bb29d8d
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2022-02-20 20:08:06 +0900

    Delete kimdokyun directory

--- kimdokyun/Python/boj_11053.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-nums = [0] + list(map(int, input().split()))
-values = [0]
-for i in range(1, N+1):
-    values.append(0)
-    for j in range(0, i):
-        if nums[j] < nums[i]:
-            values[i] = max(values[i], values[j]+1)
-print(max(values))
