d830331d8fd80a32c6485bf59c9b0981da0efebf
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2022-02-20 19:56:06 +0900

    Delete Opened directory

--- Opened/eodudrepublic/10998_A곱하기B.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-a, b = map(int, sys.stdin.readline().split())
-print(a * b)
--- Opened/lysuk96/ETC/LTC_24.py
+++
@@ -1,9 +0,0 @@
-import ListNode
-
-def swapPairs(self, head:ListNode)->ListNode:
-    cur = head
-    while cur and cur.next:
-        cur.val, cur.next.val= cur.next.val, cur.val
-        cur = cur.next.next
-
-    return head
