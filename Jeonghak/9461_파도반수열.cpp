982cd11602732c93e59db8c55668ffa12f6241e6
JeonghakLee <55916802+JeonghakLee@users.noreply.github.com>
2022-04-13 18:10:58 +0900

    [BOJ] 파도반수열 / 실버3 / .

--- Jeonghak/9461_파도반수열.cpp
+++ Jeonghak/9461_파도반수열.cpp
@@ -8,15 +8,19 @@ int main()
     int Q[101];
     long long DP[101];
     DP[0] = DP[1] = DP[2] = 1;
+    
     for(int i=0; i<T; i++){
         cin >> N;
         Q[i] = N;
         Max = max(Max, N);
     }
+    
     for(int i=3; i<Max; i++){
         DP[i] = DP[i-2] + DP[i-3];
     }
+    
     for(int i=0; i<T; i++){
         cout << DP[Q[i]-1] << endl;
     }
+    
 }
