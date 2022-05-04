c193a2e89c27d117c1f9bac79a2588d84c6db527
shasuri <ghimmk@gmail.com>
2022-05-05 01:23:36 +0900

    [PGS] 거리두기 확인하기 / LEVEL 2 / 1시간
    map이 편하게 주어져 있어서 dfs로 편하게 풀 수 있었다.
    조기 반환 검증 조건은
    - 이미 거리두기가 안된 응시자가 발견되었는가?
    - 깊이(맨해튼 거리)가 2초과인가?
    - 행과 열의 index가 범위(5)를 벗어나는가?
    - 이미 방문한 자리인가?
    등이 있다.
    
    시간 줄이는 요소를 많이 생각해 봤는데,
    - place를 인자가 아닌 전역변수로 둔다면?
    - visit을 bitmap이 아닌 배열로 둔다면?
    - 전역 flag(distanceWatcher)를 두지 않고, &연산으로 일일히 return값을 검증한다면?
    
    등을 생각해볼 수 있었다. 내일 다 시험해봐야지...

---
+++ shasuri/19th_week/2_test.cpp
@@ -0,0 +1,12 @@
+#include "2021_kakao_intern_2.cpp"
+
+int main(void)
+{
+    vector<vector<string>> testPlace =
+        {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"},
+         {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
+         {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
+         {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},
+         {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
+    solution(testPlace);
+}
