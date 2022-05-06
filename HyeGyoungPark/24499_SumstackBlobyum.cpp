6e422112e8fce021043ae2d43df3b5da41c98e78
HyeGyoungPark <64782678+HyeGyoungPark@users.noreply.github.com>
2022-05-07 01:56:58 +0900

    [BOJ] blobyum / 실버4 / 37분
    https://www.acmicpc.net/problem/24499
    
    누적 합, 슬라이딩 윈도우

---
+++ HyeGyoungPark/24499_SumstackBlobyum.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void){
+    int n, k, now=0,premax=0;
+
+    cin >> n >> k;
+
+    int* pie = new int[n+n-1];
+    int* sum = new int[n+n-1];
+
+    for(int i=0; i<n; i++){
+        scanf("%d",&pie[i]);
+    }
+    for(int i=n; i<n+n-1; i++){
+        pie[i] = pie[i-n];
+    }
+
+    for(int i=0; i<n+n-1; i++){
+        if(i==0) { sum[i] = pie[i]; }
+        else { sum[i] = sum[i-1]+pie[i]; }
+    }
+
+    for(int i=0; i<n+n-1-k; i++){
+        if(sum[i+k]-sum[i] > premax) { premax = sum[i+k]-sum[i]; }
+    }
+
+    for(int i=0; i<k; i++){
+        now+=pie[i];
+    }
+
+    if(premax<now) premax = now;
+
+
+    cout << premax;
+
+    delete[] pie;
+    delete[] sum;
+
+}
