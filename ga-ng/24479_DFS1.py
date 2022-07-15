d03a3f51ea30e1a55036a5a68aba3511d62c976e
Ga-ng <kounghun0328@gmail.com>
2022-07-15 22:09:17 +0900

    [BOJ] DFS1 / 실버2 / 1h30m

---
+++ ga-ng/24479_DFS1.py
@@ -0,0 +1,30 @@
+def dfs(r):
+    global cnt
+    
+    visited[r] = cnt
+    cnt += 1
+    
+    graph[r].sort()
+    
+    for i in graph[r]:
+        if visited[i] == 0:
+            # cnt += 1
+            dfs(i)
+
+
+n, m, r = map(int, input().split())
+
+graph = [[]for _ in range(n+1)]
+visited = [0]*(n+1)
+cnt = 1
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    
+    graph[a].append(b)
+    graph[b].append(a)
+
+dfs(r)
+
+for i in visited[1:]:
+    print(i)
