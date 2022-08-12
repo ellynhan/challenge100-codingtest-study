0c834831c8d6a221bbb8ddf18e6edd2d7161111f
Sangbeom Heo <mansaout@gmail.com>
2022-08-13 02:35:21 +0900

    [PGS] 문자열을 정수로 바꾸기 / 레벨1 / 0분
    https://school.programmers.co.kr/learn/courses/30/lessons/12925

---
+++ sangbeomheo/PGS_12925_문자열을-정수로-바꾸기.js
@@ -0,0 +1,15 @@
+/*
+
+프로그래머스 / 문자열을 정수로 바꾸기 / 레벨 1
+https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+< 소요 시간 >
+0분
+
+<프로그래밍 계획>
+  n길이 크기의 배열 생성 => 배열 인덱스가 짝수면 '수', 홀수면 '짝
+*/
+
+function solution(s) {
+  return Number(s);
+}
