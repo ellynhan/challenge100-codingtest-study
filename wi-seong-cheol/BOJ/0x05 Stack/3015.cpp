98275a9230bd926b8c7ce258562eff6aab9de251
Your Name <tjd9248@naver.com>
2022-11-07 17:42:32 +0900

    [BOJ] 오아시스 재결합 / 골드1 / 19분 https://www.acmicpc.net/problem/3015

--- wi-seong-cheol/BOJ/0x05 Stack/3015.cpp
+++ wi-seong-cheol/BOJ/0x05 Stack/3015.cpp
@@ -7,23 +7,51 @@
 
 /*
  [Input]
- 
+ 7
+ 2
+ 4
+ 1
+ 2
+ 2
+ 5
+ 1
  [Output]
- 
+ 10
  */
-// 시간복잡도: O()
-// 최악시간:
-// 난이도:
-// Timer:
-// Url: https://www.acmicpc.net/problem/
+// 시간복잡도: O(nlogn)
+// 최악시간: 3,000,000
+// 난이도: Gold 1
+// Timer: 19m
+// Url: https://www.acmicpc.net/problem/3015
 
 #include <iostream>
+#include <stack>
 
 using namespace std;
+#define X first
+#define Y second
+#define ll long long
+int n;
+ll ans;
+stack<pair<int, int>> S;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    
+    cin >> n;
+    while(n--) {
+        int x; cin >> x;
+        int cnt = 1;
+        while(!S.empty() && S.top().X <= x) {
+            ans += S.top().Y;
+            if(S.top().X == x) cnt += S.top().Y;
+            S.pop();
+        }
+        if(!S.empty()) ans++;
+        S.push({x, cnt});
+    }
+    cout << ans;
     
     return 0;
 }
