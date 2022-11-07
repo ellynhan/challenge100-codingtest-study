5aae3acbecef10aa64309ac03d2e28969cf1a38f
Your Name <tjd9248@naver.com>
2022-11-07 15:22:55 +0900

    [BOJ] 옥상 정원 꾸미기 / 골드5 / 30분 https://www.acmicpc.net/problem/6198

---
+++ wi-seong-cheol/BOJ/0x05 Stack/6198.cpp
@@ -0,0 +1,50 @@
+//
+//  6198.cpp
+//  main
+//
+//  Created by wi_seong on 2022/11/07.
+//
+
+/*
+ [Input]
+ 6
+ 10
+ 3
+ 7
+ 4
+ 12
+ 2
+ [Output]
+ 5
+ */
+// 시간복잡도: O(nlogn)
+// 최악시간: 400000
+// 난이도: Gold 5
+// Timer: 30m
+// Url: https://www.acmicpc.net/problem/6198
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+#define ll long long
+stack<ll> S;
+int n;
+ll ans;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    while(n--) {
+        ll h; cin >> h;
+        while(!S.empty() && S.top() <= h)
+            S.pop();
+        ans += S.size();
+        S.push(h);
+    }
+    cout << ans;
+    
+    return 0;
+}
