0637194b690d03f7566dced06297818a4dcbe550
Your Name <tjd9248@naver.com>
2022-11-07 14:30:29 +0900

    [BOJ] 탑 / 골드5 / 18분 https://www.acmicpc.net/problem/2493

---
+++ wi-seong-cheol/BOJ/0x05 Stack/3015.cpp
@@ -0,0 +1,29 @@
+//
+//  3015.cpp
+//  main
+//
+//  Created by wi_seong on 2022/11/07.
+//
+
+/*
+ [Input]
+ 
+ [Output]
+ 
+ */
+// 시간복잡도: O()
+// 최악시간:
+// 난이도:
+// Timer:
+// Url: https://www.acmicpc.net/problem/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    return 0;
+}
