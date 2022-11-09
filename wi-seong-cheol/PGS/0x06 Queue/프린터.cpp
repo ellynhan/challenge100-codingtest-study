0d303194d87c0722ce3e06536bd08a9a71f3b6d7
Your Name <tjd9248@naver.com>
2022-11-09 17:16:35 +0900

    [PGS] 프린터 / Level2 / 20분
    https://school.programmers.co.kr/learn/courses/30/lessons/42587

--- wi-seong-cheol/PGS/0x06 Queue/프린터.cpp
+++ wi-seong-cheol/PGS/0x06 Queue/프린터.cpp
@@ -5,4 +5,53 @@
 //  Created by wi_seong on 2022/11/09.
 //
 
-#include <stdio.h>
+/*
+ [Input]
+ 
+ [Output]
+ 
+ */
+// 시간복잡도: O(nlogn)
+// 최악시간: 200
+// 난이도: Level 2
+// Timer: 20m
+// Url: https://school.programmers.co.kr/learn/courses/30/lessons/42587
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+#define X first
+#define Y second
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    
+    queue<pair<int, int>> Q;
+    priority_queue<int> pq;
+    for(size_t i = 0; i < priorities.size(); i++) {
+        Q.push({i, priorities[i]});
+        pq.push(priorities[i]);
+    }
+    
+    int cnt = 0;
+    while(!Q.empty()) {
+        int idx = Q.front().X;
+        int val = Q.front().Y;
+        Q.pop();
+        if(pq.top() == val) {
+            pq.pop();
+            cnt++;
+            if(idx == location) {
+                answer = cnt;
+                break;
+            }
+        } else {
+            Q.push(make_pair(idx, val));
+        }
+    }
+    
+    return answer;
+}
