860a0fc2025db954a6d6dafa0da6d6e7b1b7e054
Your Name <tjd9248@naver.com>
2022-11-14 21:28:41 +0900

    [BOJ] 유기농 배추 / 실버2 / 15분 https://www.acmicpc.net/problem/1012

--- wi-seong-cheol/BOJ/0x09 BFS/1012.cpp
+++ wi-seong-cheol/BOJ/0x09 BFS/1012.cpp
@@ -7,15 +7,36 @@
 
 /*
  [Input]
- 
+ 2
+ 10 8 17
+ 0 0
+ 1 0
+ 1 1
+ 4 2
+ 4 3
+ 4 5
+ 2 4
+ 3 4
+ 7 4
+ 8 4
+ 9 4
+ 7 5
+ 8 5
+ 9 5
+ 7 6
+ 8 6
+ 9 6
+ 10 10 1
+ 5 5
  [Output]
- 
+ 5
+ 1
  */
 // 시간복잡도: O(n * m)
 // 최악시간: 2,500
 // 난이도: Silver 2
 // Timer: 15m
-// Url: https://www.acmicpc.net/problem/
+// Url: https://www.acmicpc.net/problem/1012
 
 #include <iostream>
 #include <queue>
