c150f4f28783a31bc8120f26866b84ce42e49b3f
Your Name <tjd9248@naver.com>
2022-11-14 21:28:05 +0900

    [BOJ] 유기농 배추 / 실버2 / 15분 https://www.acmicpc.net/problem/1012

---
+++ wi-seong-cheol/BOJ/0x09 BFS/1012.cpp
@@ -0,0 +1,80 @@
+//
+//  1697.cpp
+//  main
+//
+//  Created by wi_seong on 2022/11/14.
+//
+
+/*
+ [Input]
+ 
+ [Output]
+ 
+ */
+// 시간복잡도: O(n * m)
+// 최악시간: 2,500
+// 난이도: Silver 2
+// Timer: 15m
+// Url: https://www.acmicpc.net/problem/
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+#define X first
+#define Y second
+const int SIZE = 51;
+int n, m, k;
+int board[SIZE][SIZE];
+bool vis[SIZE][SIZE];
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+bool OOB(int x, int y) { return x < 0 || x >= n || y < 0 || y >= m; }
+
+void bfs(int x, int y) {
+    queue<pair<int, int>> Q;
+    Q.push({x, y});
+    vis[x][y] = true;
+    while(!Q.empty()) {
+        auto cur = Q.front(); Q.pop();
+        for(int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if(OOB(nx, ny) || vis[nx][ny]) continue;
+            if(board[nx][ny] == 0) continue;
+            vis[nx][ny] = true;
+            Q.push({nx, ny});
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) {
+        int ans = 0;
+        cin >> n >> m >> k;
+        queue<pair<int, int>> Q;
+        for(int i = 0; i < n; i++) {
+            fill(board[i], board[i] + m + 1, 0);
+            fill(vis[i], vis[i] + m + 1, false);
+        }
+        for(int i = 0; i < k; i++) {
+            int x, y; cin >> x >> y;
+            board[x][y] = 1;
+        }
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(board[i][j] == 1 && !vis[i][j]) {
+                    bfs(i, j);
+                    ans++;
+                }
+        cout << ans << '\n';
+    }
+    
+    return 0;
+}
