05c0bfb90fc3dc6ab12dcf3d11f2c43cc4227820
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2022-12-11 15:42:31 +0900

    Delete yongjunleeme directory

--- yongjunleeme/numeric_strings_and_english_words.py
+++
@@ -1,6 +0,0 @@
-def solution(s):
-    words = ["zero", "one", "two", "three", "four",
-             "five", "six", "seven", "eight", "nine"]
-    for i in range(10):
-        s = s.replace(words[i], str(i))
-    return int(s)
