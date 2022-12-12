bedb8992afec35c8866d2ee38d69879774773f04
c0np4nn4 <c0np4nn4@gmail.com>
2022-12-12 20:07:52 +0900

    [BOJ] 1, 2, 3 더하기 / 실버 3 / 5분 https://www.acmicpc.net/problem/9095

---
+++ c0np4nn4/boj/cpp/slv1_9934/complete_binary_tree.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int k = 0;
+int num_seq[1024];
+
+// tree
+int main() {
+  cin >> k;
+
+  vector<int> num_seq;
+
+  for (int i = 0; i < pow(2, k); i++) {
+    int tmp;
+    cin >> tmp;
+  }
+
+  fill_out()
+
+
+
+  return 0;
+}
