d2ce2c6cc8404351dba13de52dda92bbc6b7fc49
ahma0 <dsd932@naver.com>
2023-03-24 16:13:58 +0900

    [PGS] 가장 가까운 같은 숫자 / lv1 / 11분
    https://school.programmers.co.kr/learn/courses/30/lessons/142086

--- ahma0/PGS_142086.java
+++ ahma0/PGS_142086.java
@@ -1,22 +1,21 @@
 import java.util.*;
 
 public class Solution {
-
     public int[] solution(String s) {
+        int[] answer = new int[s.length()];
         LinkedHashMap<Character, Integer> map = new LinkedHashMap();
-        ArrayList<Integer> arrayList = new ArrayList();
 
         for(int i = 0; i < s.length(); i++) {
             if(!map.containsKey(s.charAt(i))) {
                 map.put(s.charAt(i), i);
-                arrayList.add(-1);
+                answer[i] = -1;
                 continue;
             }
-            arrayList.add(i - map.get(s.charAt(i)));
+            answer[i] = i - map.get(s.charAt(i));
             map.put(s.charAt(i), i);
         }
 
-        return arrayList.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 
 }
