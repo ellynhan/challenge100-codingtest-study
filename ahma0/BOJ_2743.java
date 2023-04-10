7e3a63740d1e5be308c48f1a2b2c3b00152adcca
ahma0 <dsd932@naver.com>
2023-04-10 23:46:17 +0900

    [BOJ] 단어 길이 재기 / 브론즈5 / 3분
    https://www.acmicpc.net/problem/2743

---
+++ ahma0/BOJ_2743.java
@@ -0,0 +1,13 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+
+}
