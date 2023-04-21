1d1c98a815dd129b41b04740de795ec4f067fadd
Hayoung Noh <40683453+watertree34@users.noreply.github.com>
2023-04-22 02:14:52 +0900

    [LIC] Running Sum of 1d Array / Easy / 4분
    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/

---
+++ HayoungNoh/0420_[LIC]Running Sum of 1d Array: 홀짝반복문.cpp
@@ -0,0 +1,11 @@
+class Solution {//0420 11:50~11:54
+public:
+    int numberOfSteps(int num) {
+        int result =0;
+        while(num>=1){
+            num = num%2==0 ? num/2 : num-1;
+            result++;
+        }
+        return result;
+    }
+};
