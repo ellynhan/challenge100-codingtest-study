173487eba6ddc6d9390677530a0e0fe2c87b9d39
Hayoung Noh <40683453+watertree34@users.noreply.github.com>
2023-04-22 01:58:04 +0900

    [LIC] Running Sum of 1d Array / Easy / 3분

---
+++ HayoungNoh/0417_[LIC]Running Sum of 1d Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> result;
+        int num = 0;
+        for(int i=0;i<nums.size();i++){
+            result.push_back(num+=nums[i]);
+        }
+        return result;
+    }
+};
+
+// 배운 것: 1차원 벡터
