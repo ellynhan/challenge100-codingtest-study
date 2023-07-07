fea017ddbb97aeccaaa48b7d0bf10845ab782d06
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-07-07 11:04:34 +0900

    [BOJ] / 24460 / silver3 / 43분

--- wlwl1011/DivideConquer_24460.py
+++ wlwl1011/DivideConquer_24460.py
@@ -11,27 +11,24 @@ atexit.register(lambda: os.write(1, stdout.getvalue()))
 #이 규칙을 재귀적으로 적용해서 구역마다 한 명씩 총 네 명을 뽑는다.
 #뽑힌 네 명 중 의자에 적힌 추첨번호가 두 번째로 작은 사람이 뽑힌다.
 
-N = int(input())
+def special_award(x, y, n):
 
+    if n == 1:
+        return arr[x][y]
+    else:
+        n = n // 2
+        second_minimum = [special_award(x,y,n),special_award(x,y+n,n),special_award(x+n,y,n),special_award(x+n,y+n,n)]
+        second_minimum.sort()
+        return second_minimum[1]
+
+N = int(input())
 arr = [ [0 for i in range(N)] for i in range(N) ]
 
 
 for i in range(N):
     arr[i] = list(map(int, input().split()))
 
-arr = []
-def special_award(x_start, x_end, y_start, y_end):
-    if x_start >= x_end:
-        return    
-    if x_start-x_end == 2:
-        new_arr = []
-        for i in range(x_start, x_end):
-            for j in range(y_start, y_end):
-                new_arr.append(arr[i][j])
-        return arr.append(new_arr[1])
-    special_award(x_start//2, x_end//2, y_start//2, y_end//2)
-    special_award(x_start, x_end//2, y_start//2, y_end//2)
-    
 
+print(special_award(0, 0, N))
 
 
