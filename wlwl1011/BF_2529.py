e726c060f1f1cd5a41b08240ef410fce804c323e
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-07-12 11:23:20 +0900

    [BOJ] 2529 / silver1 / 59분 (Refactoring)

--- wlwl1011/BF_2529.py
+++ wlwl1011/BF_2529.py
@@ -1,67 +1,35 @@
-import sys, os, io, atexit
-import copy
-from collections import deque
-input = lambda: sys.stdin.readline().rstrip('\r\n')
-stdout = io.BytesIO()
-sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
-atexit.register(lambda: os.write(1, stdout.getvalue()))
+from sys import stdin
+input = stdin.readline
 
-def dfs(st , number, inequality):
+k = int(input())
+sign = list(input().split())
 
-    if len(st) > k+1:
+visited = [0]*10
+max_ans = ''
+min_ans = ''
+
+def check(i,j,k):
+    if k == '<':
+        return i < j
+    else:
+        return i > j
+    
+def solve(depth,s):
+    global max_ans, min_ans
+
+    if depth == k+1:
+        if len(min_ans) == 0:
+            min_ans = s
+        else:
+            max_ans = s
         return
-    if len(st) == k+1: #우리는 k+1의 개수만큼만 숫자만 필요하니께
-        answer_list.append(st)
-        return 
-    if arr[inequality] == '<':
-       
-        #number 보다 큰 수가 와야함
-        for i in range(10):
-          
-            if str(i) in st:
-                continue
-            if number >i: #만약 작은수면 저리가셈
-                continue 
-            #위의 경우가 아니라면 새로운 숫자로 임명될 자격이 있다!
-            temp_st = st
-            st += str(i)
-           
-            dfs(st, i,inequality+1)
-            st = temp_st
-            
-    elif arr[inequality] == '>':
-        
-        #작은 수 와야함        
-          for i in range(9):
-            if str(i) in st:
-                continue
-            if number < i: #만약 큰 수면 저리가셈
-                continue 
-            #위의 경우가 아니라면 새로운 숫자로 임명될 자격이 있다!
-            temp_st = st
-            st += str(i)
-            dfs(st, i, inequality+1)
-            st = temp_st
-               
+    for i in range(10) :
+        if not visited[i]:
+            if depth == 0 or check(s[-1], str(i), sign[depth-1]):
+                visited[i] = True
+                solve(depth+1, s+str(i))
+                visited[i] = False            
 
-k = int(input()) #부등호 문자의 개수
-
-arr = list(map(str,input().split()))
-
-
-
-
-global answer_list
-answer_list = []
-
-i = 0
-    #그럼 숫자를 골라보자
-for index in range(10):
-    s = ''
-    s += str(index)
-    dfs(s, index,i)
-
-
-answer_list.sort(key = lambda x : int(x))
-print(answer_list[len(answer_list)-1])
-print(answer_list[0])
+solve(0,"")
+print(max_ans)
+print(min_ans)                
