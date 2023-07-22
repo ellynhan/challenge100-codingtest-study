11bf7f6ac2a27988ab1b6d2067aaeb248f6a183c
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-07-22 11:03:29 +0900

    [BOJ] 13460 / gold1 / 2일

--- wlwl1011/BF_13460.py
+++ wlwl1011/BF_13460.py
@@ -1,141 +1,53 @@
-import sys, os, io, atexit
-input = lambda : sys.stdin.readline().rstrip('\r\n')
-stdout = io.BytesIO()
-sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
-atexit.register(lambda: os.write(1,stdout.getvalue()))
+from sys import stdin
 from collections import deque
+input = stdin.readline
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+n, m = map(int, input().split())
+a = [list(input().strip()) for _ in range(n)]
+check = [[[[False]*m for _ in range(n)] for _ in range(m)] for _ in range(n)]
+dx, dy = (-1, 0, 1, 0), (0, 1, 0, -1)
+q = deque()
 
-global answer
+def init():
+    _rx, _ry, _bx, _by = [0]*4
+    for i in range(n):
+        for j in range(m):
+            if a[i][j] == 'R':
+                _rx, _ry = i, j
+            elif a[i][j] == 'B':
+                _bx, _by = i, j
+    q.append((_rx, _ry, _bx, _by, 0))
+    check[_rx][_ry][_bx][_by] = True
 
+def move(_x, _y, _dx, _dy, _c):
+    while a[_x+_dx][_y+_dy] != '#' and a[_x][_y] != 'O':
+        _x += _dx
+        _y += _dy
+        _c += 1
+    return _x, _y, _c
 
-def isBarrier(x, y):
-    if arr[x][y] == '#':
-        return True
-    else:
-        return False    
-
-def isInNumber(x,y):
-    if 0< x < N-1 and 0 <= y < M-1:
-        return True
-    else:
-        return False
-
-def isHold(x,y):
-    if arr[x][y] == 'O':
-        return True
-    else:
-        return False
-
-def move(r_x, r_y, b_x, b_y, direction, depth):
-    check_red_change = False
-    check_blue_change = False
-    if depth > 10:
-        return
-    # print("------------")
-    # print('depth:', depth)
-    # print('direction:', direction)
-    # print('red: ' ,r_x, r_y )
-    # print('blue: ', b_x, b_y )
-    global answer
-
-    red_in_hold = False
-    blue_in_hole = False
-    # print('moveing~')   
-
-    while True:
-        tr_x = r_x + dx[direction]
-        tr_y = r_y + dy[direction]
-
-        if isInNumber(tr_x, tr_y) == False or isBarrier(tr_x, tr_y) == True:
+def bfs():
+    while q:
+        rx, ry, bx, by, d = q.popleft()
+        if d >= 10:
             break
-        elif isHold(tr_x, tr_y):
-            r_x = 0
-            r_y = 0
-            red_in_hold = True
-            break
-        elif tr_x == b_x and tr_y == b_y:
-            break
-        else:
-            check_red_change = True
-            r_x = tr_x
-            r_y = tr_y
-            visited[r_x][r_y] = 1
-            
-    while True:    
-        tb_x = b_x + dx[direction]
-        tb_y = b_y + dy[direction]
-
-        if isInNumber(tb_x, tb_y) == False or isBarrier(tb_x, tb_y) == True:
-            break
-        elif isHold(tb_x, tb_y):
-            b_x= 0
-            b_y =0
-            blue_in_hole = True
-            break
-        elif tb_x == r_x and tb_y == r_y:
-            break
-        else:
-            check_blue_change = True
-            b_x = tb_x
-            b_y = tb_y
-            visited[b_x][b_y] = 1
-
-    # print('red: ' ,r_x, r_y )
-    # print('blue: ', b_x, b_y )     
-    if red_in_hold == True and blue_in_hole == False:
-        answer = min(answer, depth)
-        return
-    elif red_in_hold == True and blue_in_hole == True:
-        answer = -1
-        return
- 
-    
-    if not check_red_change and not check_blue_change: #변경된거 없음
-        return
-
-    for i in range(4):      
-        tr_x = r_x + dx[i]
-        tr_y = r_y + dy[i]
-
-        if isInNumber(tr_x, tr_y) == False or isBarrier(tr_x, tr_y) == True:
-            continue
-        if visited[tr_x][tr_y] == 0:
-            move(r_x, r_y, b_x, b_y, i, depth+1) 
-                   
-      
-
-answer = int(1e9)
-N, M = map(int, input().split()) #세로 가로
-arr = [[0 for _ in range(M)] for _ in range(N)]
-visited =  [[0 for _ in range(M)] for _ in range(N)]
-
-red_x, red_y, blue_x, blue_y = 0, 0, 0, 0
-for i in range(N):
-    temp = input()
-    arr[i] = temp
-    for j in range(M):
-        if temp[j] == 'R':
-            red_x = i
-            red_y= j
-        elif temp[j] == 'B':
-            blue_x = i
-            blue_y = j
-            # print(i,j)
-        elif temp[j] == '#':
-            visited[i][j] = 1    
-
-for i in range(4):    
-    # print('\n',i,"start\n")
-    visited =  [[0 for _ in range(M)] for _ in range(N)]
-    move(red_x, red_y, blue_x, blue_y,  i, 1)
-
-
-# print('answer is..... ')
-# print(answer)
-if answer > 10:
+        for i in range(4):
+            nrx, nry, rc = move(rx, ry, dx[i], dy[i], 0)
+            nbx, nby, bc = move(bx, by, dx[i], dy[i], 0)
+            if a[nbx][nby] == 'O':
+                continue
+            if a[nrx][nry] == 'O':
+                print(d+1)
+                return
+            if nrx == nbx and nry == nby:
+                if rc > bc:
+                    nrx, nry = nrx-dx[i], nry-dy[i]
+                else:
+                    nbx, nby = nbx-dx[i], nby-dy[i]
+            if not check[nrx][nry][nbx][nby]:
+                check[nrx][nry][nbx][nby] = True
+                q.append((nrx, nry, nbx, nby, d+1))
     print(-1)
-else:
-    print(answer)    
+
+init()
+bfs()
