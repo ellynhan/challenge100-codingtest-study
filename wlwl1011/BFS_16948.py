680f0918d36bf2d82322f56754dbe3c2a5aaaa9a
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-07-27 10:20:43 +0900

    [BOJ] 16948 / silver1 / 25분 (범위 수정)

--- wlwl1011/BFS_16948.py
+++ wlwl1011/BFS_16948.py
@@ -5,8 +5,8 @@ sys.stdout.write = lambda s : stdout.write(s.encode("ascii"))
 atexit.register(lambda: os.write(1, stdout.getvalue()))
 from collections import deque
 
-dx = [-2,-2,0,0,2,2]
-dy = [-1,1,-2,2,-1,1]
+dx = [-2, -2, 0, 0, 2, 2]
+dy = [-1, 1, -2, 2, -1, 1]
 
 N = int(input())
 arr = [ [0 for _ in range(N)] for _ in range(N)]
@@ -18,15 +18,17 @@ queue.append((r1,c1))
 
 while queue:
     x, y = queue.popleft()
-    if x == r2 and y == c2:
-        break
+ 
     for i in range(6):
         cx = x + dx[i]
         cy = y + dy[i]
-        if 0<= cx < N-1 and 0<= cy < N-1:
+        if 0<= cx < N and 0<= cy < N:
             if visited[cx][cy] == 0:
                 visited[cx][cy] = visited[x][y] + 1
                 queue.append((cx,cy))
+
+# for i in range(N):
+#     print(visited[i])
 
 if visited[r2][c2] == 0:
     print(-1)
