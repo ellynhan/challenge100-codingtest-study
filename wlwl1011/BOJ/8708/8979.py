99cdd2505a31b061df7ad892733fe78baed92b8d
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-08-31 12:55:51 +0900

    [BOJ] 8979 / silver5 / 30분

--- wlwl1011/BOJ/8708/8979.py
+++ wlwl1011/BOJ/8708/8979.py
@@ -12,31 +12,11 @@ for i in range(N):
     arr[i] = [num, gold,silver,bronze]
 
 arr.sort(key=lambda x: (x[1],x[2],x[3]), reverse=True)
-check = [0] * (N+1)
-answer = 1
-check[1] = 1
-# print('...................')
-# for i in range(N):
-#     print(arr[i])
 
-for i in range(1, N):
-    
-    if not (arr[i][1] == arr[i-1][1] and arr[i][2] == arr[i-1][2] and arr[i][3] == arr[i-1][3]):
-        if arr[i][1] < arr[i-1][1]: #금메달 수가 더 적으면
-            answer += check[answer]
-        else: #금메달 수가 같으면
-            #은메달 수가 더 적으면
-            if arr[i][2] < arr[i-1][2]:
-                answer += check[answer]
-            else: #은메달 수가 같으면
-                #동메달 수가 더 적으면
-                if arr[i][3] < arr[i-1][3]:
-                    answer += check[answer]
-                else:
-                    #동메달 수가 같으면
-                    continue     
-    if arr[i][0] == K:
-        break  
-    check[answer] += 1    
-    #print(check)    
-print(answer)
+#국가 K에 해당하는 index 찾기
+idx = [arr[i][0] for i in range(N)].index(K)
+
+for i in range(N):
+    if arr[idx][1:] == arr[i][1:]:
+        print(i+1)
+        break
