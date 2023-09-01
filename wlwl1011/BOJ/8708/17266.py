c029d1079f169e36a4df5447b4d34875b6b6de16
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-09-01 15:56:17 +0900

    [BOJ] 17266 / silver4 / 30분

--- wlwl1011/BOJ/8708/17266.py
+++ wlwl1011/BOJ/8708/17266.py
@@ -4,12 +4,27 @@ stdout = io.BytesIO()
 sys.stdout.write = lambda s : stdout.write(s.encode("ascii"))
 atexit.register(lambda : os.write(1, stdout.getvalue()))
 
-N = int(input())
-M = int(input())
-arr = list(map(int, input().split()))
+def bs(li, m):
+    print(li,m)
+    if li[1]-li[0] > m:
+        return 0
+    if li[-1]-li[-2] > m:
+        return 0
+    for i in range(1, len(li)-2):
+        if (li[i+1]-li[i])/2 > m:
+            return 0
+    return 1
 
-max_size = 0
-for i in range(1,M):
-    max_size = max(max_size, arr[i] - arr[i-1])
-
-print(max((max_size+1)//2,arr[0] - 0, N - arr[-1]))
+N, M = int(input()), int(input())
+li = [0] + list(map(int, input().split())) + [N]
+s, e = 0, N
+res = 0
+while s <= e:
+    m = (s+e)//2
+    #print(res)
+    if bs(li, m):
+        e = m-1
+        res = m
+    else:
+        s = m+1
+print(res)
