e2164df79d59540c60903f178733950169050413
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-09-01 15:42:55 +0900

    [BOJ] 17266 / silver4 / 30분

--- wlwl1011/BOJ/8708/17266.py
+++ wlwl1011/BOJ/8708/17266.py
@@ -4,42 +4,12 @@ stdout = io.BytesIO()
 sys.stdout.write = lambda s : stdout.write(s.encode("ascii"))
 atexit.register(lambda : os.write(1, stdout.getvalue()))
 
-#굴다리의 길이
 N = int(input())
-
-#가로등의 개수
 M = int(input())
+arr = list(map(int, input().split()))
 
-#가로등의 위치
-x = list(map(int,input().split()))
+max_size = 0
+for i in range(1,M):
+    max_size = max(max_size, arr[i] - arr[i-1])
 
-arr = [0] * (N+1)
-start = 0
-end = 0
-for i in range(1,N+1):
-    temp = []
-    for index in x:
-        #왼쪽
-        if index - i <= 0 :
-            start = 0
-        else:
-            start = index - i 
-        if len(temp) > 0:
-            if start - temp[-1] <= 0: 
-                temp.append(start)
-            else:
-                break
-        else:
-            temp.append(start)    
-
-        #오른쪽
-        if index + i >= N :
-            end = N 
-        else:
-            end = index + i
-        temp.append(end)    
-
-    #print(temp)
-    if temp[0] == 0 and temp[-1] >= N:
-        print(i)
-        break
+print(max((max_size+1)//2,arr[0] - 0, N - arr[-1]))
