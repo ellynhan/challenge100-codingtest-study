c72add31fe0b5b6ea2276005588b2a9cc9d8dfdc
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-09-07 10:27:47 +0900

    [BOJ] 8708 / silver1 / 1일일

--- wlwl1011/BOJ/8708/1522.py
+++ wlwl1011/BOJ/8708/1522.py
@@ -5,28 +5,13 @@
 # atexit.register(lambda : os.write(1, stdout.getvalue()))
 
 arr = list(input())
+numberOfA=arr.count('a')
+arr = arr + arr[:numberOfA-1]
 
-start = 0
-end = len(arr)-1
+min_val = int(1e9)
+for i in range(len(arr)):
+    if i+numberOfA > len(arr):
+        break
+    min_val =  min(min_val,arr[i:i+numberOfA].count('b'))
 
-if arr[start] == arr[end]: #처음 양 싸이클이 같은 경우
-    start += 1
-    end -= 1
-else:
-    end -= 1
-count = 0    
-while start <= end:
-    print(arr)
-    if arr[start] == 'a':
-        start += 1
-    else: #만약 b 면은 뒤에 있는 a랑 바꾸자
-        if arr[end] == 'b':    
-            arr[start], arr[end] = arr[end], arr[start]
-        else:
-            while True:
-                if arr[end] == 'b':
-                    break
-                end -= 1 #앞으로 한칸 땡겨오그레이  
-            arr[start], arr[end] = arr[end], arr[start]    
-    count += 1
-print(count)    
+print(min_val)
