c6ac2dd57621c3da0da75f7cb593c41c666fc832
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-09-11 11:06:12 +0900

    [BOJ] 13549 / gold5 / 20분

--- wlwl1011/BOJ/8708/13549.py
+++ wlwl1011/BOJ/8708/13549.py
@@ -15,18 +15,18 @@ visited[N] = 0
 while queue:
     time = queue.popleft()
     if time == K:
-        print(visited[time])
         break
-    if time*2 < 100001 and visited[time*2] == -1:
-        visited[time*2] = visited[time]
-        queue.append(time*2)
-    if time+1 < 100001 and visited[time+1] == -1:    
-        visited[time+1] = visited[time]+1
-        queue.append(time+1)
-    if time-1 < 100001 and visited[time-1] == -1:    
+    if 0 <= time-1 < 100001 and visited[time-1] == -1:    
         visited[time-1] = visited[time]+1
         queue.append(time-1)
+    if 0 <= time*2 < 100001 and visited[time*2] == -1:
+        visited[time*2] = visited[time]
+        queue.append(time*2)
+    if 0 <= time+1 < 100001 and visited[time+1] == -1:    
+        visited[time+1] = visited[time]+1
+        queue.append(time+1)
+ 
         
-
+print(visited[time])
 
 
