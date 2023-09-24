3b9f72548381088a6248fe44bfb9a14ef83cdc5f
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-09-24 10:35:51 +0900

    [BOJ] 11497 / silver1 / 17분

---
+++ wlwl1011/BOJ/heybob/1회/11497.py
@@ -0,0 +1,29 @@
+import sys, os, io, atexit
+input = lambda : sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s : stdout.write(s.encode("ascii"))
+atexit.register(lambda : os.write(1, stdout.getvalue()))
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    new = [0] * N
+    i,j = 0,N-1
+    for index in range(N):
+        if index % 2 == 0:
+            new[i] = arr[index]
+            i += 1
+        else:
+            new[j] = arr[index]    
+            j -= 1
+            
+    #print(new)        
+    ans = -int(1e9)       
+    for i in range(1,N):
+        ans = max(ans, abs(new[i-1]-new[i]) )
+    ans = max(ans, abs(new[-1]-new[0]) )
+    print(ans)    
+
