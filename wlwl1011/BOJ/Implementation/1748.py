c6a57300bf0e2fde1feb83413d224ceaa125ae65
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-09-26 19:58:41 +0900

    [BOJ] 3085 / silver2 / 30분

--- wlwl1011/BOJ/Implementation/1748.py
+++ wlwl1011/BOJ/Implementation/1748.py
@@ -6,7 +6,10 @@ answer = 0
 
 for i in range(comp):
     answer += 9 * (10 ** i) * (i + 1)
+    print(9 * (10 ** i) * (i + 1))
     i += 1
-answer += ((int(n) - (10 ** comp)) + 1) * (comp + 1)
+print(((int(n) - (10 ** comp)) + 1))
 
-print(answer)
+answer += ((int(n) - (10 ** comp)) + 1) * (comp + 1)
+#print(((int(n) - (10 ** comp)) + 1))
+print(answer) 
