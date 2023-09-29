1af4946d8526d0847b28cf5a0cdfbc742ddb81cb
wlwl1011 <minji001011@naver.com>
2023-09-29 13:38:04 +0900

    [BOJ] 2161 / silver5 / 6분

--- wlwl1011/BOJ/Implementation/2477.py
+++ wlwl1011/BOJ/Implementation/2477.py
@@ -11,7 +11,7 @@ K = int(input())
 
 for i in range(6):
     direction, length = map(int, input().split())
-    arr.append(list(direction,length))
+    arr.append([direction,length])
     if direction == 1 or direction == 2:
         width.append(length)
     else:
@@ -22,4 +22,4 @@ for i in range(6):
     if arr[i][0] == arr[(i+2)%6][0]:
         small.append(arr[(i+1)%6][1])
 
-print((max(height)*max(width)-(small[0]*small[1]))
+print((max(height)*max(width)-(small[0]*small[1]))*K)
