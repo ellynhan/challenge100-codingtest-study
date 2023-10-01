e38861917c90a958ec6ebd1bbaaeb2df1b8e2c69
wlwl1011 <minji001011@naver.com>
2023-10-01 11:11:27 +0900

    [BOJ] 9251 / Gold4 / 12분

--- wlwl1011/BOJ/heybob/1회/13869.py
+++ wlwl1011/BOJ/heybob/1회/13869.py
@@ -3,14 +3,17 @@ import math
 def calculate_area(S):
     N = len(S)
     S.sort()
-    new_order = [S[-1]]  # 가장 큰 값을 중앙에
-
-    for i in range(1, N):
-        if i % 2 == 1:
-            new_order.insert(0, S[-(i+1)])
+    new_order = [0 for _ in range(N)]  # 가장 큰 값을 중앙에
+    start = 0
+    end = N-1
+    for i in range( N):
+        if i % 2 == 0:
+            new_order[start] = S[i]
+            start +=1
         else:
-            new_order.append(S[-(i+1)])
-
+            new_order[end] = S[i]
+            end -= 1
+    #print(new_order)
     area = 0.0
     angle = 2 * math.pi / N
     for i in range(N):
