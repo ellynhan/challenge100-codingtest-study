89b703be0238d2335c65d35748b920e3981eab6a
wlwl1011 <minji001011@naver.com>
2023-10-18 00:25:32 +0900

    [BOJ] 2812 / gold3 / 1시간

--- wlwl1011/BOJ/LG/17298.py
+++ wlwl1011/BOJ/LG/17298.py
@@ -1,17 +1,19 @@
 import sys
-
 input = lambda : sys.stdin.readline().rstrip('\r\n')
 
 N = int(input())
+seq = list(map(int, input().split()))
+s = []
+ans = [0 for _ in range(N)]
+for i in range(N-1,-1,-1):
+    
+    while s and s[-1] <= seq[i]:
+        s.pop()
+    if not s:
+        ans[i] = -1
+    else:
+        ans[i] = s[-1]
+      
+    s.append(seq[i])            
 
-arr = list(map(int, input().split()))
-stack = []
-answer = [-1]*N
-
-stack.append(0)
-for i in range(1,N):
-    while stack and arr[stack[-1]] < arr[i]:
-        answer[stack.pop()] = arr[i]
-    stack.append(i)
-
-print(*answer)
+print(*ans)
