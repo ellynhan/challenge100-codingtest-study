98a85d3a82b7a773d63b5a8ead44eca80aeeb90d
JiYeon Jeong <stopkite98@naver.com>
2024-01-04 01:06:43 +0900

    [LTC] Two Sum / Easy / 5분

---
+++ stopkite/041_TwoSum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
