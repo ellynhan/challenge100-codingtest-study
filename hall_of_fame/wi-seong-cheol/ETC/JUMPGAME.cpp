a49524df657f3ef8daf8e9357a7f8c2961b30b6d
wi-seong-cheol <tjd9248@naver.com>
2024-01-10 17:06:44 +0900

    [ETC] JUMPGAME / 하 / 10분
    https://algospot.com/judge/problem/read/JUMPGAME

---
+++ hall_of_fame/wi-seong-cheol/ETC/JUMPGAME.cpp
@@ -0,0 +1,29 @@
+//
+//  JUMPGAME.cpp
+//  main
+//
+//  Created by wi_seong on 1/10/24.
+//
+
+/*
+ [Input]
+ 
+ [Output]
+ 
+ */
+// 시간복잡도: O()
+// 최악시간: 
+// 난이도: 
+// Timer: 
+// Url: https://www.acmicpc.net/problem/JUMPGAME
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	return 0;
+}
