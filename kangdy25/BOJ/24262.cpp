7952382698cc540619cc76e088de9facf50525e1
Shineast <canfrog@naver.com>
2024-03-02 23:00:05 +0900

    알고리즘 수업 - 알고리즘의 수행 시간1 / 브론즈5 / 1분
    https://www.acmicpc.net/problem/24262

---
+++ kangdy25/BOJ/24262.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int o;
+    cin >> o;
+    
+    cout << "1" << "\n" << "0";
+}
