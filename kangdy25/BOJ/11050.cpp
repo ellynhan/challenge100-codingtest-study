c4ee70dd06bb2158cab6ffc8de9a46012c95c558
Shineast <canfrog@naver.com>
2024-04-30 11:30:38 +0900

    [BOJ] 이항 계수 1 / 브론즈1 / 7분
    https://www.acmicpc.net/problem/11050

---
+++ kangdy25/BOJ/11050.cpp
@@ -0,0 +1,25 @@
+// 이항 계수 1 / 브론즈 1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, K, result = 1;
+    cin >> N >> K;
+    int NK = N - K; 
+    
+
+    for (int i = N; i >= 1; i--) {
+        result *= i;
+    }
+    for (int j = K; j >= 1; j--) {
+        result /= j;
+    }
+    for (int j = NK; j >= 1; j--) {
+        result /= j;
+    }
+    cout << result << "\n";
+}
